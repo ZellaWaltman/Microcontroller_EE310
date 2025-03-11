182e231fccc68d379acb31a6e59ede25ff0befbf
ZellaWaltman <Waltmanz@sonoma.edu>
2025-03-10 19:42:10 -0700

    Initializing Signal_Gen lab

---
+++ Assignments/Signal_Generator_LAB/Signal_Gen.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Signal_Gen.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Signal_Gen.X.production.hex
